36f15b02f69d5462c5cac8e2603cdc8b236e9f8f
sologuboved <sologuboved@gmail.com>
2020-03-11 23:17:33 +0300

    just another initial commit

---
+++ bot.py
@@ -0,0 +1,6 @@
+from helpers import notify_of_alert
+
+
+if __name__ == '__main__':
+    notify_of_alert('test')
+
---
+++ poem_obtainer.py
@@ -0,0 +1,7 @@
+# поэтика.рф/поэты/сумароков/сборники-стихов/все
+# <div id="author-verse" class="content-block ">
+
+
+def collect_century():
+    pass
+
