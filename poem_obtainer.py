7c8fa1c28bdda7d180178b3d12d6b0d7770dfb67
sologuboved <sologuboved@gmail.com>
2020-03-12 19:59:33 +0300

    scrapes random poems

--- poem_obtainer.py
+++ poem_obtainer.py
@@ -1,7 +1,41 @@
-# поэтика.рф/поэты/сумароков/сборники-стихов/все
-# <div id="author-verse" class="content-block ">
+import random
+import requests
+from bs4 import BeautifulSoup
+
+_prefix = 'http://поэтика.рф'
 
 
-def collect_century():
-    pass
+# random.seed(1)
 
+
+def grab_random(items):
+    item = items[random.randrange(len(items))].find('a')
+    return item.get_text().strip(), item.get('href').strip()
+
+
+def choose_poet(period):
+    return grab_random(BeautifulSoup(
+        requests.get(_prefix + '/раздел/' + period).content, 'lxml'
+    ).find('div', {'id': 'period-authors'}).find_all('li'))
+
+
+def choose_title(period):
+    name, link_postfix = choose_poet(period)
+    return name, grab_random(BeautifulSoup(
+        requests.get(_prefix + link_postfix + '/сборники-стихов/все').content, 'lxml'
+    ).find_all('li', {'class': 'node-item'}))
+
+
+def scrape_poem(period):
+    name, (title, link_postfix) = choose_title(period)
+    link = _prefix + link_postfix
+    return link, name, title, BeautifulSoup(
+        requests.get(link).content, 'lxml'
+    ).find('div', {'class': "content clearfix"}).text
+
+
+def get_poems():
+    return [scrape_poem(period) for period in (
+        'Поэзия-XVIII-века',
+        'Золотой-век-русской-поэзии',
+        ('Поэзия-Серебряного-века', 'Поэзия-советского-периода')[random.randrange(3) % 2])]
