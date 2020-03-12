e160c2d0cb70886721f94dfb13c11ca0fceb3bba
sologuboved <sologuboved@gmail.com>
2020-03-12 20:06:25 +0300

    added bot

--- bot.py
+++ bot.py
@@ -1,6 +1,16 @@
-from helpers import notify_of_alert
+from telegram import Bot, ParseMode
+from poem_obtainer import get_poems
+from poem_processor import process_poem
+from tkn import TOKEN, GROUP_ID
+
+
+def send_poems():
+    bot = Bot(token=TOKEN)
+    for poem in get_poems():
+        bot.send_message(chat_id=GROUP_ID,
+                         text=process_poem(*poem),
+                         parse_mode=ParseMode.HTML)
 
 
 if __name__ == '__main__':
-    notify_of_alert('test')
-
+    send_poems()
