c0b57e3898d82e5a880ffc2c9a42037f936c8519
sologuboved <sologuboved@gmail.com>
2020-03-12 20:24:40 +0300

    changed formatting

--- poem_processor.py
+++ poem_processor.py
@@ -1,2 +1,2 @@
 def process_poem(link, name, title, poem):
-    return "{} - {}\n\n{}\n\n{}".format(name, title, poem, link)
+    return '<b>{}</b> - <a href="{}">{}</a>\n\n{}'.format(name, link, title, poem.strip())
